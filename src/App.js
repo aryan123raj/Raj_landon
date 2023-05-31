d78ece2bbe884d5eaf629dfc24bae8d821a66f7b
Raj Singh <aryanav8349@gmail.com>
2023-05-31 12:42:54 +0530

    Updating the boilerplate App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React in a new way and deploy to aws
         </a>
       </header>
     </div>
