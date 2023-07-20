d2dc8196b4eaa2909f4de48156ad95a70587aabc
Raj Singh <aryanav8349@gmail.com>
2023-07-20 20:16:34 +0530

    Updating the App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React in a new way and deploy to aws
+          Learn React in a new way Aryan
         </a>
       </header>
     </div>
